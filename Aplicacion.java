e081c12d940abd3541c09c9c36774841b4d073ca
Tere <teresaolivaresrueda@gmail.com>
2026-06-09 20:31:04 -0400

    es par

---
+++ Aplicacion.java
@@ -0,0 +1,5 @@
+public class Aplicacion {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ clases/Algoritmo.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Algoritmo {
+  public static boolean esPar(int numero) {
+    if (numero % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
