797a74b4605ff93d5a17d27731765ccaeaf889c2
Tere <teresaolivaresrueda@gmail.com>
2026-06-09 20:49:41 -0400

    Es primo

--- Aplicacion.java
+++ Aplicacion.java
@@ -1,5 +1,16 @@
+import clases.Algoritmo;
+
 public class Aplicacion {
   public static void main(String[] args) {
+    // Prueba Metodo esPar
+    System.out.println("\nPrueba metodo esPar");
+    System.out.println(Algoritmo.esPar(45));
+    System.out.println(Algoritmo.esPar(400));
 
+    // Prueba esPrimo
+    System.out.println("\nPrueba es Primo");
+    System.out.println(Algoritmo.esPrimo(1));
+    System.out.println(Algoritmo.esPrimo(2));
+    System.out.println(Algoritmo.esPrimo(37));
   }
 }
--- clases/Algoritmo.java
+++ clases/Algoritmo.java
@@ -1,12 +1,27 @@
 package clases;
 
 public class Algoritmo {
+
   public static boolean esPar(int numero) {
+    // tambien pudo ir solo: return numero % 2 == 0;
     if (numero % 2 == 0) {
       return true;
     } else {
       return false;
     }
+  }
+
+  public static boolean esPrimo(int numero) {
+    // numeros menores a 2
+    if (numero < 2) {
+      return false;
+    }
+    for (int i = 2; i < numero; i++) {
+      if (numero % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 
 }
